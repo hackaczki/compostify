17b515ab73e5e1c7071add98be77ed20b3a8988b
Kajetan Knopp <kajetan@knopp.com.pl>
2022-11-20 04:22:55 +0100

    Commit 2

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import dash
 from dash import dcc, html
 import flask
+from flask.templating import render_template
 from data_processing import *
 import plotly.express as px
+from flask import Flask, flash, render_template, redirect, request, url_for, jsonify, session, Response
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 server = flask.Flask(__name__)
+
+server.secret_key = 'grogjreelfijreijreerjt'
+server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+UPLOAD_FOLDER = 'static/plants'
+ALLOWED_EXTENSIONS = {'jpg'}
+server.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+db = SQLAlchemy(server)
+
+class Posts(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.Integer, nullable=False)
+    Author = db.Column(db.Integer, nullable=False)
+    Text = db.Column(db.String(50))
+    Date = db.Column(db.String(100), nullable=False)
+    Reply_to = db.Column(db.Integer, nullable=True)
+    Photo = db.Column(db.String(20), nullable=True)
+
+    def __repr__(self):
+        return f"Post('{self.Id}', '{self.Title}', '{self.Author}')"
+
+class Users(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(50))
+    Password = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f"User('{self.Id}', '{self.Username}')"
+
+with server.app_context():
+    db.create_all()
+
+
 
 colors = {
     'background': '#111111',
@@ -33,14 +70,14 @@ app = dash.Dash(server=server, routes_pathname_prefix="/dash/")
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Hello Dash',
+        children='Compostify!',
         style={
             'textAlign': 'center',
             'color': colors['text']
         }
     ),
 
-    html.Div(children='Dash: A web application framework for your data.', style={
+    html.Div(children='Compostify: A web application framework for your ECO trash.', style={
         'textAlign': 'center',
         'color': colors['text']
     }),
@@ -49,6 +86,16 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     dcc.Graph(figure=mapfig)
     ])
 
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
+
+@server.route('/forum')
+def forum():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run_server(debug=True)
